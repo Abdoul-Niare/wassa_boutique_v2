1e8b78288fff299ae119d09ae67509cfb4e6027e
Abdoul-Niare <108657768+Abdoul-Niare@users.noreply.github.com>
2023-04-07 00:23:09 +0200

    mise en place du systeme panier

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Products;
+use App\Repository\CategoriesRepository;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository, CategoriesRepository $categoriesRepository): Response
     {
-        return $this->render('products/index.html.twig');
+        
+        return $this->render('products/index.html.twig', [
+            'products'=>$productsRepository->findAll(),
+            // 'category'=> $categoriesRepository->findAll()
+        ]);
     }
+
+
 
     #[Route('/{slug}', name: 'details')]
     public function details(Products $product): Response
